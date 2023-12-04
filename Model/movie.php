e29fbf142d81e92c2595ca88be4ec5d57619ba3b
albertostizzoli <albertostizzoli@yahoo.it>
2023-12-04 15:43:09 +0100

    Create file card

--- Model/movie.php
+++ Model/movie.php
@@ -3,15 +3,17 @@ class Movie
 {
     private int $id;
     private string $title;
+    private string $original_title;
     private string $overview;
     private float $vote_average;
     private string $poster_path;
     private string $original_language;
 
-    public function __construct($id, $title, $overview, $vote, $image, $language)
+    public function __construct($id, $title, $subtitle, $overview, $vote, $image, $language)
     {
         $this->id = $id;
         $this->title = $title;
+        $this->original_title = $subtitle;
         $this->overview = $overview;
         $this->vote_average = $vote;
         $this->poster_path = $image;
@@ -19,11 +21,11 @@ class Movie
     }
 
     public function getVote()
-    {   
+    {
         $vote = ceil($this->vote_average / 2);
         $template = "<p>";
         for ($n = 1; $n <= 5; $n++) {
-            $template .= $n <= $vote ? '<i class="fa-solid fa-star"></i>' :  '<i class="fa-regular fa-star"></i>';
+            $template .= $n <= $vote ? '<i class="fa-solid fa-star"></i>' : '<i class="fa-regular fa-star"></i>';
         }
         $template .= "</p>";
         return $template;
@@ -33,6 +35,7 @@ class Movie
     {
         $image = $this->poster_path;
         $title = $this->title;
+        $subtitle = $this->original_title;
         $content = substr($this->overview, 0, 100) . '...';
         $custom = $this->getVote();
         $language = $this->original_language;
@@ -45,6 +48,6 @@ $movieList = json_decode($movieString, true);
 
 $movies = [];
 foreach ($movieList as $item) {
-    $movies[] = new Movie($item['id'], $item['title'], $item['overview'], $item['vote_average'], $item['poster_path'], $item['original_language']);
+    $movies[] = new Movie($item['id'], $item['title'], $item['original_title'], $item['overview'], $item['vote_average'], $item['poster_path'], $item['original_language']);
 }
 ?>
--- index.php
+++ index.php
@@ -10,13 +10,20 @@ include __DIR__ . '/Model/movie.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
-    integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>PHP-OOP-1</title>
 </head>
 <body>
-
+    <h1 class="text-center text-white bg-danger">MOVIES</h1>
+    <div class="container">
+        <div class="row gy-4">
+            <?php foreach ($movies as $movie) {
+                $movie->printCard();
+            } ?>
+        </div>
+    </div>
 </body>
 </html>
